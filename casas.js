4c4c0747edb11bee087d7c5d31e4ee2a4ba8a2ca
André Filipe Sousa  Barreto <barreto.andre@discente.ufma.br>
2022-03-05 13:15:49 -0300

    aplicação de javaScript para animações

---
+++ casas.js
@@ -0,0 +1,96 @@
+
+
+
+mudaCor_Gryf = ()=>{
+    var corp = document.getElementById("corpo");
+    corp.style.backgroundImage="linear-gradient(45deg, rgba(209,15,15,1) 2%, rgba(158,26,26,1) 8%, rgba(166,31,31,1) 14%, rgba(126,8,8,1) 20%, rgba(126,8,8,1) 61%, rgba(126,8,8,1) 70%, rgba(126,8,8,1) 72%, rgba(191,16,16,1) 94%)";
+
+    var casa = document.getElementById("Cabeça")
+    var brasao = document.getElementById("Brasão")
+    var fundador = document.getElementById("Fundador")
+    var cores = document.getElementById("Cores")
+    var valores = document.getElementById("Valores")
+   
+    casa.innerText="Grifinória";
+    brasao.innerText="Brasão: Leão"
+    fundador.innerText="Fundador: Godric Gryffindor"
+    cores.innerText="Cores: Vermelho e dourado"
+    valores.innerText="Valores: Coragem, Lealdade, Ousadia"
+
+    
+
+}
+
+mudaCor_Sly = ()=>{
+    var corp = document.getElementById("corpo");
+    corp.style.backgroundImage="linear-gradient(45deg, rgba(15,209,17,1) 2%, rgba(26,158,44,1) 8%, rgba(31,166,53,1) 14%, rgba(8,126,9,1) 20%, rgba(8,126,9,1) 61%, rgba(8,126,9,1) 70%, rgba(8,126,9,1) 72%, rgba(16,191,17,1) 94%)";
+    var casa = document.getElementById("Cabeça")
+    var brasao = document.getElementById("Brasão")
+    var fundador = document.getElementById("Fundador")
+    var cores = document.getElementById("Cores")
+    var valores = document.getElementById("Valores")
+   
+    casa.innerText="Sonserina";
+    brasao.innerText="Brasão: Serpente"
+    fundador.innerText="Fundador: Salazar Slytherin"
+    cores.innerText="Cores: Verde"
+    valores.innerText="Valores: Ambição, sagacidade, Liderança"
+    
+
+}
+
+mudaCor_hup = ()=>{
+    var corp = document.getElementById("corpo");
+    corp.style.backgroundImage="linear-gradient(45deg, rgba(209,158,15,1) 2%, rgba(209,151,14,1) 8%, rgba(166,133,31,1) 14%, rgba(149,132,43,1) 20%, rgba(126,105,8,1) 61%, rgba(126,105,8,1) 70%, rgba(125,115,19,1) 90%, rgba(128,114,11,1) 90%)";
+
+    var casa = document.getElementById("Cabeça")
+    var brasao = document.getElementById("Brasão")
+    var fundador = document.getElementById("Fundador")
+    var cores = document.getElementById("Cores")
+    var valores = document.getElementById("Valores")
+   
+    casa.innerText="Lufa-Lufa";
+    brasao.innerText="Brasão: Texugo"
+    fundador.innerText="Fundador: Helga Hufflepuff"
+    cores.innerText="Cores: Amarelo"
+    valores.innerText="Valores: Lealdade, justiça e paciência"
+    
+
+}
+
+mudaCor_rav = ()=>{
+    var corp = document.getElementById("corpo");
+    corp.style.backgroundImage="linear-gradient(45deg, rgba(21,15,209,1) 2%, rgba(22,24,163,1) 8%, rgba(31,68,166,1) 14%, rgba(43,52,149,1) 20%, rgba(8,22,126,1) 61%, rgba(8,50,126,1) 70%, rgba(19,32,125,1) 88%, rgba(11,43,128,1) 94%)";
+
+    var casa = document.getElementById("Cabeça")
+    var brasao = document.getElementById("Brasão")
+    var fundador = document.getElementById("Fundador")
+    var cores = document.getElementById("Cores")
+    var valores = document.getElementById("Valores")
+   
+    casa.innerText="Corvinal";
+    brasao.innerText="Brasão: Águia"
+    fundador.innerText="Fundador: Rowena Ravenclaw"
+    cores.innerText="Cores: Azul e cinza"
+    valores.innerText="Valores: Sabedoria, inteligência e Criatividade"
+    
+
+}
+
+mudaCor_origin = () =>{
+    var corp = document.getElementById("corpo");
+    var brasao = document.getElementById("Brasão")
+    var fundador = document.getElementById("Fundador")
+    var cores = document.getElementById("Cores")
+    var valores = document.getElementById("Valores")
+    corp.style.backgroundImage="linear-gradient(to right, #0B1413,#0B1413)";
+    
+    var casa = document.getElementById("Cabeça");
+    casa.innerHTML="";
+    brasao.innerText="";
+    fundador.innerText="";
+    cores.innerText="";
+    valores.innerText="";
+   
+}
+
